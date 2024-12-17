f012f2405217d44a21cb80e319097719d9d3f3a3
haiku1135 <haiku1135@gmail.com>
2024-12-18 07:51:16 +0900

    [add]HonoとCloudflareWorkersでTodoApp作成途中

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,16 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { Hono } from "hono";
+import { basicAuth } from "hono/basic-auth";
+import { todos } from "./todos/api";
 
-export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
-	},
-} satisfies ExportedHandler<Env>;
+const app = new Hono();
+
+app.use(
+  "/api/*",
+  basicAuth({
+    username: "charizard",
+    password: "super-secret",
+  })
+);
+app.route("/api/todos", todos);
+
+export default app;
